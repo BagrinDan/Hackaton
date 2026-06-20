21e06b412641bdafce12b9d8d7cd98cb8766fa7d
BagrinD <bagrin.danu@gmail.com>
2026-06-20 17:58:28 +0300

    Fixeed nr.2 DatabaseFactory

--- services/beach/src/main/kotlin/infrastructure/database/DatabaseFactory.kt
+++ services/beach/src/main/kotlin/infrastructure/database/DatabaseFactory.kt
@@ -1,3 +1,14 @@
+package com.hackathon.summer.faf.infrastructure.database
+
+import com.hackathon.summer.faf.infrastructure.database.table.ActivityTable
+import com.hackathon.summer.faf.infrastructure.database.table.VisitorsTable
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.config.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
 object DatabaseFactory {
     fun init(config: ApplicationConfig) {
         val rawUrl = System.getenv("JDBC_URL") ?: config.property("database.jdbcUrl").getString()
