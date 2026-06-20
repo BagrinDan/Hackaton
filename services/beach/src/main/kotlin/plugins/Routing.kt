2f2f16971375b4c78d04060ba0f5270afe242ecf
Anastasia11111111114_ML <zabolotnay.dev@gmail.com>
2026-06-21 02:49:55 +0300

    Add use cases for adding and removing activities

--- services/beach/src/main/kotlin/plugins/Routing.kt
+++ services/beach/src/main/kotlin/plugins/Routing.kt
@@ -1,7 +1,9 @@
 package com.hackathon.summer.faf.plugins
 
+import com.hackathon.summer.faf.application.usecase.AddActivityUseCase
 import com.hackathon.summer.faf.application.usecase.BookActivityUseCase
 import com.hackathon.summer.faf.application.usecase.CancelActivityUseCase
+import com.hackathon.summer.faf.application.usecase.RemoveActivityUseCase
 import com.hackathon.summer.faf.infrastructure.repository.PostgresActivityRepository
 import com.hackathon.summer.faf.infrastructure.repository.PostgresVisitorRepository
 import com.hackathon.summer.faf.presentation.controller.ActivityController
@@ -13,11 +15,19 @@ fun Application.configureRouting() {
 
     val activityRepository = PostgresActivityRepository()
     val visitorRepository = PostgresVisitorRepository()
+
     val bookUseCase = BookActivityUseCase(activityRepository, visitorRepository)
-
     val cancelUseCase = CancelActivityUseCase(activityRepository)
+    val addUseCase = AddActivityUseCase(activityRepository)
+    val removeUseCase = RemoveActivityUseCase(activityRepository)
 
-    val controller = ActivityController(activityRepository, bookUseCase, cancelUseCase)
+    val controller = ActivityController(
+        activityRepository,
+        bookUseCase,
+        cancelUseCase,
+        addUseCase,
+        removeUseCase
+    )
 
     routing {
         activityRoutes(controller)
