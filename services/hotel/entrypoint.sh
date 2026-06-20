0a45126b2e3a78e9e822dff7150cc8faf70c58da
BagrinD <bagrin.danu@gmail.com>
2026-06-20 16:52:49 +0300

    Fixed Prisma issue with database.url

--- services/hotel/entrypoint.sh
+++ services/hotel/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
-
 echo "DATABASE_URL=$DATABASE_URL"
-
-pnpm exec prisma migrate deploy
+pnpm exec prisma migrate deploy --config prisma.config.ts
 node dist/prisma/seed.js
 exec node dist/src/main.js
